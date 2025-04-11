ed6a6a7e7fef068b1fa2fcff9053fb406ca663f4
Azizul Islam <azizultushar98@gmail.com>
2025-04-12 02:42:58 +0600

    Add user profile update and retrieve all trainees routes

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -2,15 +2,19 @@ import { Router } from "express";
 import { protect } from "../../middleware/protect";
 import {
   createTrainer,
+  getAllTrainees,
   getAllTrainers,
   getCurrentUser,
+  updateUserProfile,
 } from "./user.controller";
 import { restrictTo } from "../../middleware/restrictTo";
 
 const router = Router();
 
 router.get("/me", protect, getCurrentUser);
+router.put("/profile", protect, updateUserProfile);
 router.post("/trainers", protect, restrictTo("Admin"), createTrainer);
 router.get("/trainers", protect, restrictTo("Admin"), getAllTrainers);
+router.get("/trainees", protect, restrictTo("Admin"), getAllTrainees);
 
 export default router;
